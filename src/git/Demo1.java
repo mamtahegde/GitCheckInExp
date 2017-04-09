95d86802470310c23eed4035de948b902d98a977
Mamta-PC 2 <Mamta-PC@DESKTOP-5FH3MB0>
2017-04-09 23:52:02 +0530

    Something

--- src/git/Demo1.java
+++ src/git/Demo1.java
@@ -7,6 +7,8 @@ public class Demo1 {
 	@Test
 	public void testA(){
 		Reporter.log("This is the creation of framework",true);
+		Reporter.log("This is the imported one",true);
+		Reporter.log("Thank you so much",true);
 	}
 	
 
