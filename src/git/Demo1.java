ea68525f56f6e661d084485ab93068bf3312ae82
Mamta-PC <Mamta-PC@DESKTOP-5FH3MB0>
2017-04-09 23:50:50 +0530

    Added some comments

--- src/git/Demo1.java
+++ src/git/Demo1.java
@@ -7,6 +7,9 @@ public class Demo1 {
 	@Test
 	public void testA(){
 		Reporter.log("This is the creation of framework",true);
+		Reporter.log("This is the original one",true);
+		Reporter.log("Be careful",true);
+		//Have a nice day
 	}
 	
 
