c3fe24263bab27f15f51200a6cf1c7f04883a363
Mamta-PC <Mamta-PC@DESKTOP-5FH3MB0>
2017-04-09 23:32:08 +0530

    Uploading Initial code

---
+++ src/git/Demo1.java
@@ -0,0 +1,13 @@
+package git;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	@Test
+	public void testA(){
+		Reporter.log("This is the creation of framework",true);
+	}
+	
+
+}
